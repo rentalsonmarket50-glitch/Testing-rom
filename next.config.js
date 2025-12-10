b0026cd23456b905faad5ce1e2776eab7eb40cd3
Base2Brand <techbase2brand@gmail.com>
2025-12-10 15:04:46 +0530

    Fix: Add cdn-icons-png.flaticon.com to Next.js image config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ module.exports = {
         protocol: 'https',
         hostname: 'links.papareact.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
